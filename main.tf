07cb675d561832ca77353386b46860e308326ae8
Tadius Frank <tadiusfrank2001@gmail.com>
2025-08-24 09:42:33 -0400

    Cloud Provider and Data Sources

---
+++ main.tf
@@ -0,0 +1,31 @@
+# Main Terraform configuration
+
+# Provider
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+# Data sources
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+# VPC
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name = "secure-vpc"
+  }
+}
