f2a8b6a20bc995962b558fa61e5675eb8a845222
Tadius Frank <tadiusfrank2001@gmail.com>
2025-08-26 19:24:08 -0400

    revert creds

--- outputs.tf
+++ outputs.tf
@@ -45,7 +45,7 @@ output "security_monitoring" {
 
 output "team_credentials" {
   description = "Team member credentials (store securely)"
-  sensitive   = false
+  sensitive   = true
   value = {
     red_team = {
       for i, member in local.red_team_members : member.username => {
